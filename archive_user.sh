8520fdd2cdb3ecc708866b9e9699a44ecaaec5ba
fluxbloom <michaelserafin2@gmail.com>
2023-10-18 13:23:52 -0500

    added archive comments

--- archive_user.sh
+++ archive_user.sh
@@ -4,15 +4,12 @@ for i in $@
 do 
     echo "Executing script: $0"
     echo "Archiving user: $i"
+# Lock account
     passwd -l $i
+# Check if target dir exists/ make dir
     [ ! -d /home/archives/${i} ] && echo "directory to path does NOT exist, creating now..."
     mkdir -p /home/archives/${i}
-    
+# Back up verbose, preserve permissions w/datestamp    
     tar -zcvpf /home/archives/${i}-backup-$(date +%d-%m-%Y).tar.gz /home/${i} 
 done
 
-# Lock the account
-#passwd -l $1
-
-# Create an archive of the home directory
-#tar -cf /archives/${1}.tar.gz /home/${1} 
